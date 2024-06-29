1e1a68ddfa8d774ad5f9ef864d44abd662a9a8b8
zhangtwe <98892280+zhangtwe@users.noreply.github.com>
2024-06-29 19:26:42 +0800

    Add files via upload

---
+++ test_1.cmd
@@ -0,0 +1,4 @@
+@echo off
+.\aria2c.exe --max-concurrent-downloads=4^
+ -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"^
+ --input-file=urls.txt
